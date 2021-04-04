2cce2db52b41c14e9c5dfec4b65133d4b4b94aeb
muhmadjaradat <muhmadjradat@gmail.com>
2021-04-05 00:32:25 +0300

    creating the locale storage

---
+++ js/collection.js
@@ -0,0 +1,56 @@
+'use strict';
+let cartCount=0;
+let addToCart=document.getElementsByClassName('addToCartBtn');
+let cartNum=document.querySelector('body > header > div > nav > ul > li:nth-child(2) > a');
+let productID=[];
+
+// console.log(addToCart);
+let list=[];
+let storageList=[];
+
+for (let i = 0; i < addToCart.length; i++) {
+  addToCart[i].addEventListener('click',function(e){
+    productID.push(addToCart[i].firstChild.id);
+    // console.log(productID);
+    // if () {
+    //   cartCount++;
+    // }
+    let quantity=0;
+console.log(localStorage.length);
+    if (localStorage.length===0) {
+
+      list.push({productID: addToCart[i].firstChild.id,productName: addToCart[i].parentElement.parentElement.children[1].textContent,productNum: addToCart[i].parentElement.parentElement.children[2].children[0].textContent,productQuantity: quantity+1});
+      localStorage.setItem('name',JSON.stringify((list)));
+    }
+
+    else {
+      let found = false;
+      for(let j = 0; j < list.length; j++) {
+        if (list[j].productID === e.target.id) {
+          found = true;
+          break;
+        }
+      }
+      if (found){
+        storageList=JSON.parse(localStorage.getItem('name'));
+        list=storageList;
+        list[i].productQuantity++;
+        console.log(list[i].productQuantity);
+        
+      localStorage.setItem('name',JSON.stringify((list)));
+      }
+      
+      else {
+      storageList=JSON.parse(localStorage.getItem('name'));
+      list=storageList;
+      list.push({productID: addToCart[i].firstChild.id,productName: addToCart[i].parentElement.parentElement.children[1].textContent,productNum: addToCart[i].parentElement.parentElement.children[2].children[0].textContent,productQuantity: quantity+1});
+      localStorage.setItem('name',JSON.stringify((list)));
+      console.log(found);}
+    }
+    cartNum.textContent=`Cart (${cartCount})`;
+   });
+
+}
+
+
+
