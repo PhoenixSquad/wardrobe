5c48ca3233eba0434daaa0c8cbf0a78339c548b2
Yaser Al-Ananbeh <yaser.ananbeh74@gmail.com>
2021-04-06 23:53:21 +0300

    Edit the sub nav style in collection page

--- js/yaser.js
+++ js/yaser.js
@@ -12,10 +12,13 @@ function showMenProducts(event){
   event.preventDefault();
   menProduct.style.display='block';
   menFilter.style.background='#284b63';
+  menFilter.style.color='white';
   womenProduct.style.display='none';
   womenFilter.style.background='white';
+  womenFilter.style.color='black';
   kidsProduct.style.display='none';
   kidsFilter.style.background='white';
+  kidsFilter.style.color='black';
 
 }
 womenFilter.addEventListener('click',showWomenProducts);
@@ -23,10 +26,13 @@ function showWomenProducts(event){
   event.preventDefault();
   womenProduct.style.display='block';
   womenFilter.style.background='#284b63';
+  womenFilter.style.color='white';
   menProduct.style.display='none';
   menFilter.style.background='white';
+  menFilter.style.color='black';
   kidsProduct.style.display='none';
   kidsFilter.style.background='white';
+  kidsFilter.style.color='black';
 
 }
 kidsFilter.addEventListener('click',showKidsProducts);
@@ -34,9 +40,12 @@ function showKidsProducts(event){
   event.preventDefault();
   kidsProduct.style.display='block';
   kidsFilter.style.background='#284b63';
+  kidsFilter.style.color='white';
   womenProduct.style.display='none';
   womenFilter.style.background='white';
+  womenFilter.style.color='black';
   menProduct.style.display='none';
   menFilter.style.background='white';
+  menFilter.style.color='black';
 }
 
